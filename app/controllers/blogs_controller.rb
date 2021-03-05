c2fee52f6e833d5999ae8ea386589ac6b0fdf249
kmtszky <kmtszk711@gmail.com>
2021-03-05 02:20:52 +0000

    [Update] 3-6 : blogs_controller > updateアクションを定義

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -21,7 +21,12 @@ class BlogsController < ApplicationController
     @blog = Blog.find(params[:id])
   end
 
-  private
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog.id)
+  end
+
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
